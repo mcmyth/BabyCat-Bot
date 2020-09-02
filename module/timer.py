a1b9a691fc1f9a4dc549347edf68b22d75b3df11
MC_Myth <1440126177@qq.com>
2020-09-02 11:08:58 +0800

    Add asyncioInterval class

--- module/timer.py
+++ module/timer.py
@@ -1,7 +1,7 @@
 import time
 import asyncio
 from threading import Thread, Event
-
+from functools import partial
 
 class _Timer(Thread):
     """Call a function after a specified number of seconds:
@@ -38,7 +38,24 @@ class RepeatingTimer(_Timer):
             self.finished.wait(self.interval)
 
 
+class asyncioInterval:
+    def __run_with_wrapper(self,func, *args, **kwargs):
+        exe = func(*args, **kwargs)
+        if asyncio.iscoroutine(exe):
+            asyncio.ensure_future(exe)
 
+    def __interval(self,delay, func, *args, **kwargs):
+        self.__run_with_wrapper(func, *args, **kwargs)
+        asyncio.get_event_loop().call_later(delay, partial(self.__interval, delay, func, *args, **kwargs))
 
+    async def __call_later(self,delay, callback, *args, **kwargs):
+        return asyncio.get_event_loop().call_later(delay, partial(self.__run_with_wrapper, callback, *args, **kwargs))
 
+    def call_later(self,delay, callback, *args, **kwargs):
+        return asyncio.get_event_loop().call_later(delay, partial(self.__run_with_wrapper, callback, *args, **kwargs))
 
+    def loop(self,delay, callback, *args, **kwargs):
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(self.__call_later(delay, callback, *args, **kwargs))
+        loop.run_forever()
